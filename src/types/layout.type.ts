128e88009a8d5e500af455bb15acebd21451d4e2
pionpill <673486387@qq.com>
2024-12-16 22:28:59 +0800

    :truck: 类型文件重命名

--- src/types/layout.type.ts
+++ src/types/layout.type.ts
@@ -1,4 +1,4 @@
-export type LayoutProps = {
+export type LayoutPaddingProps = {
   padding?: string | number;
   paddingX?: string | number;
   paddingY?: string | number;
@@ -6,6 +6,9 @@ export type LayoutProps = {
   paddingRight?: string | number;
   paddingTop?: string | number;
   paddingBottom?: string | number;
+};
+
+export type LayoutMarginProps = {
   margin?: string | number;
   marginX?: string | number;
   marginY?: string | number;
@@ -14,3 +17,15 @@ export type LayoutProps = {
   marginTop?: string | number;
   marginBottom?: string | number;
 };
+
+export type LayoutBorderProps = {
+  border?: string | number;
+  borderX?: string | number;
+  borderY?: string | number;
+  borderLeft?: string | number;
+  borderRight?: string | number;
+  borderTop?: string | number;
+  borderBottom?: string | number;
+};
+
+export type LayoutProps = LayoutPaddingProps & LayoutMarginProps & LayoutBorderProps;
---
+++ src/types/shape.type.ts
@@ -0,0 +1,6 @@
+/** 几何体范围：内部，外部，边界线上 */
+export enum Area {
+    INNER = 'inner',
+    OUTER = 'outer',
+    EDGE = 'edge',
+};
