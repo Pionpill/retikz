76aaf9d626e541e819c019f1ff03d6232fcd750c
pionpill <673486387@qq.com>
2024-12-26 00:23:49 +0800

    :construction: 支持获取两点之间的垂点

--- src/components/path/SinglePath/utils.ts
+++ src/components/path/SinglePath/utils.ts
@@ -3,6 +3,7 @@ import { PointPosition } from '../../../types/coordinate';
 import { DescartesPosition, Position } from '../../../types/coordinate/descartes';
 import { PolarPosition } from '../../../types/coordinate/polar';
 
+/** 将坐标格式转换为笛卡尔坐标数组形式 */
 export const formatPointPosition = (point: PointPosition): Position => {
   if (Array.isArray(point)) return point;
   if (point.hasOwnProperty('x') && point.hasOwnProperty('y')) {
@@ -11,3 +12,10 @@ export const formatPointPosition = (point: PointPosition): Position => {
   }
   return PolarPoint.convertPolarToDescartesPosition(point as PolarPosition);
 };
+
+/** 获取两点间的垂直点 */
+export const getVerticalPoint = (point1: PointPosition, point2: PointPosition, type: '-|' | '|-'): Position => {
+  const p1 = formatPointPosition(point1);
+  const p2 = formatPointPosition(point2);
+  return type === '-|' ? [p2[0], p1[1]] : [p1[0], p2[1]];
+}
