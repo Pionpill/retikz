a4d202ac53888c0fe6b8b40d646fdc225a0fcdde
pionpill <673486387@qq.com>
2024-12-30 00:17:25 +0800

    :construction: 线段样式支持多种写法

--- src/utils/stroke.ts
+++ src/utils/stroke.ts
@@ -15,6 +15,21 @@ export type StrokeType =
   | 'denselyDashDashDot'
   | 'looselyDashDashDot';
 
+export type StrokeShortcutProps = {
+  dashed?: boolean;
+  denselyDashed?: boolean;
+  looselyDashed?: boolean;
+  dotted?: boolean;
+  denselyDotted?: boolean;
+  looselyDotted?: boolean;
+  dashDot?: boolean;
+  denselyDashDot?: boolean;
+  looselyDashDot?: boolean;
+  dashDashDot?: boolean;
+  denselyDashDashDot?: boolean;
+  looselyDashDashDot?: boolean;
+};
+
 /** 将 StrokeType 转换为 svg 原生的属性 */
 export const convertStrokeType = (strokeType: StrokeType, strokeWidth = 1): Partial<StrokeProps> => {
   switch (strokeType) {
@@ -45,4 +60,35 @@ export const convertStrokeType = (strokeType: StrokeType, strokeWidth = 1): Part
     case 'looselyDashDashDot':
       return { strokeDasharray: `${strokeWidth * 4} ${strokeWidth * 6} 1 ${strokeWidth * 6} 1 ${strokeWidth * 6}` };
   }
+};
+
+export const convertStrokeShortcut = (shortcutProps: StrokeShortcutProps, strokeWidth = 1): Partial<StrokeProps> => {
+  return convertStrokeType(
+    shortcutProps.dashed
+      ? 'dashed'
+      : shortcutProps.denselyDashed
+      ? 'denselyDashed'
+      : shortcutProps.looselyDashed
+      ? 'looselyDashed'
+      : shortcutProps.dotted
+      ? 'dotted'
+      : shortcutProps.denselyDotted
+      ? 'denselyDotted'
+      : shortcutProps.looselyDotted
+      ? 'looselyDotted'
+      : shortcutProps.dashDot
+      ? 'dashDot'
+      : shortcutProps.denselyDashDot
+      ? 'denselyDashDot'
+      : shortcutProps.looselyDashDot
+      ? 'looselyDashDot'
+      : shortcutProps.dashDashDot
+      ? 'dashDashDot'
+      : shortcutProps.denselyDashDashDot
+      ? 'denselyDashDashDot'
+      : shortcutProps.looselyDashDashDot
+      ? 'looselyDashDashDot'
+      : 'solid',
+    strokeWidth || 1,
+  );
 };
