0a45394a7a8bd18796ab0c534fb30e52683deec1
pionpill <673486387@qq.com>
2024-12-30 18:12:30 +0800

    :construction: 格式化

--- src/components/draw/common.ts
+++ src/components/draw/common.ts
@@ -1,4 +1,4 @@
-import { PointPosition } from "../../types/coordinate";
+import { PointPosition } from '../../types/coordinate';
 
 /** 垂直路径点，临近的节点不能都是特殊路径点 */
 export type VerticalDrawPosition = '-|' | '|-';
@@ -11,9 +11,9 @@ const moveReg = /\+\+[[(]?[+-]?\d+(?:\.\d+)?,\s*[+-]?\d+(?:\.\d+)?[)\]]?/;
 
 /** 获取点类型 */
 export const getDrawPointType = (point: string | PointPosition): DrawPointType => {
-    if (typeof point !== 'string') return 'coordinate';
-    if (['-|', '|-'].includes(point)) return 'vertical';
-    if (point.match(moveReg)) return 'move';
-    if (point.match(offsetReg)) return 'offset';
-    return 'node';
-  };
+  if (typeof point !== 'string') return 'coordinate';
+  if (['-|', '|-'].includes(point)) return 'vertical';
+  if (point.match(moveReg)) return 'move';
+  if (point.match(offsetReg)) return 'offset';
+  return 'node';
+};
