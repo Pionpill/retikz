1614dda9ebe355c73f05d27fc7819c53a6347c49
pionpill <673486387@qq.com>
2025-01-20 18:23:20 +0800

    :package: 初步完成打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    minify: false,
+    lib: {
+      entry: 'src/index.ts',
+      name: 'tikz',
+      fileName: '[name]',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: [
+        {
+          format: 'es',
+          dir: './dist/es',
+          exports: 'named',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+        {
+          format: 'cjs',
+          dir: './dist/lib',
+          exports: 'named',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+      ],
+    },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+    define: {
+      'process.env.NODE_ENV': '"production"',
+    },
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.json',
+      outDir: ['dist/lib', 'dist/es'],
+    }),
+  ],
+});
