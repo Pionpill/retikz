43437bd192533c680d51b0527390c0a639100c0f
pionpill <673486387@qq.com>
2026-05-31 14:24:07 +0800

    :pencil: 调整 Node 概览示例

--- apps/docs/src/contents/core/components/node/overview/node-shapes.demo.tsx
+++ apps/docs/src/contents/core/components/node/overview/node-shapes.demo.tsx
@@ -6,20 +6,20 @@ import type { FC } from 'react';
  * @description rectangle（默认）/ circle / ellipse / diamond；外围 4 个连到中央 hub，path 端点按各自 shape 自动贴边。
  */
 const Demo: FC = () => (
-  <Layout width={420} height={260}>
+  <Layout width={360} height={220}>
     <Node id="hub" position={[0, 0]}>
       hub
     </Node>
-    <Node id="rect" position={[-160, -80]}>
+    <Node id="rect" position={[-120, -60]}>
       rectangle
     </Node>
-    <Node id="circ" shape="circle" position={[160, -80]}>
+    <Node id="circ" shape="circle" position={[120, -60]}>
       circle
     </Node>
-    <Node id="elli" shape="ellipse" position={[160, 80]}>
+    <Node id="elli" shape="ellipse" position={[120, 60]}>
       ellipse
     </Node>
-    <Node id="diam" shape="diamond" position={[-160, 80]}>
+    <Node id="diam" shape="diamond" position={[-120, 60]}>
       diamond
     </Node>
     <Draw way={['rect', 'hub']} arrow="->" />
