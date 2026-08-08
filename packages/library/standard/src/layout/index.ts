16d8ab9f956e03806442e32b2d517c17bccef6b8
pionpill <673486387@qq.com>
2026-08-09 00:13:27 +0800

    ✨ standard: 开放布局组合契约
    为上层 Tier 2 owner 提供受控的 Flex 编译、布局几何与 artifact 原子公共面。
    
    Refs: module=standard; packages=standard; version=0.1.0-alpha.3; adr=diagram/notation/alpha.1/01
    Control: human-directed

---
+++ packages/library/standard/src/layout/index.ts
@@ -0,0 +1,62 @@
+/**
+ * 供 Tier 2 owner 组合使用的 Standard 布局公共面
+ *
+ * @description 只公开 canonical Flex 编译器、必要的共享布局词汇，以及通用几何和 artifact 原子能力
+ */
+export { compileFlexLayout } from '../composites/layout/flex-layout/pipeline';
+
+export {
+  alignAllocationInSlot,
+  contentRectOf,
+  createLayoutArtifactContainer,
+  createLayoutArtifactItem,
+  layoutClipOf,
+  normalizeLayoutSpacing,
+  resolveLayoutAxisSize,
+  unionLayoutArtifactRects,
+} from '../composites/layout/internal';
+
+export {
+  LayoutAlignment,
+  LayoutArtifactContainerSchema,
+  LayoutArtifactItemBaseSchema,
+  LayoutArtifactRectSchema,
+  LayoutAxisSizeKind,
+  LayoutDistribution,
+  LayoutOverflow,
+  LayoutOverflowSchema,
+  LayoutSizeSchema,
+} from '../composites/layout/shared';
+
+export { FlexLayoutDirection, FlexLayoutSchema, FlexLayoutWrap } from '../composites/layout/flex-layout';
+
+export type {
+  CreateLayoutArtifactItemInput,
+  LayoutInsets,
+  LayoutRect,
+  ResolveLayoutAxisSizeInput,
+  ResolvedLayoutAxisSize,
+} from '../composites/layout/internal';
+
+export type {
+  FlexLayoutArtifact,
+  FlexLayoutDirectionValue,
+  FlexLayoutWrapValue,
+  IRFlexLayout,
+  IRFlexLayoutItem,
+} from '../composites/layout/flex-layout';
+
+export type {
+  IRLayoutAxisSize,
+  IRLayoutSize,
+  LayoutAlignmentValue,
+  LayoutArtifactAlignmentGuide,
+  LayoutArtifactContainer,
+  LayoutArtifactItemBase,
+  LayoutArtifactOverflow,
+  LayoutArtifactRect,
+  LayoutAxisSizeKindValue,
+  LayoutDistributionValue,
+  LayoutOverflowValue,
+  LayoutSizeInput,
+} from '../composites/layout/shared';
