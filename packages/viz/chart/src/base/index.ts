9f0451553b6a0e058f96a2fc66e6e0ac12942204
pionpill <673486387@qq.com>
2026-08-14 22:11:05 +0800

    ♻️ chart: 统一 barrel 导出形式
    统一 Chart base 与 point 聚合入口的导出方式，保持 barrel 公开面由源模块自动聚合。
    
    Refs: module=chart; packages=@retikz/chart; version=-; adr=-
    Control: human-directed

--- packages/viz/chart/src/base/index.ts
+++ packages/viz/chart/src/base/index.ts
@@ -1,41 +1,5 @@
-export { ChartDefinition } from './definition';
-export type {
-  ChartAuthoringInput,
-  ChartPresentationAuthoringRecord,
-  ChartPresentationFlexItem,
-  ChartPresentationPositionValue,
-  ChartPresentationPresetValue,
-  ChartPresentationShorthand,
-  IRChartPresentation,
-  IRChartPresentationItem,
-  IRChartPresentationPlotItem,
-} from './presentation';
-export {
-  ChartPresentationAuthoringRecordSchema,
-  ChartPresentationFlexItemSchema,
-  ChartPresentationItemKey,
-  ChartPresentationItemSchema,
-  ChartPresentationPlotItemSchema,
-  ChartPresentationPosition,
-  ChartPresentationPreset,
-  ChartPresentationSchema,
-  ChartPresentationTextSchema,
-  createChart,
-  DEFAULT_CHART_DATA_REFERENCE,
-  normalizeChartPresentation,
-} from './presentation';
-export { ChartProvider, ChartProviderKey, createChartProvider } from './provider';
-export type { IRChart } from './schemas';
-export { CHART_COMPOSITE_TYPE, CHART_NAMESPACE, ChartSchema } from './schemas';
-export type {
-  ChartThemeStyleDefinition,
-  ChartThemeTokenValue,
-  IRChartResolvedThemeTokens,
-  IRChartThemeTokenOverrides,
-} from './style';
-export {
-  ChartResolvedThemeTokensSchema,
-  ChartThemeToken,
-  ChartThemeTokenOverridesSchema,
-  defineChartThemeStyle,
-} from './style';
+export * from './definition';
+export * from './presentation';
+export * from './provider';
+export * from './schemas';
+export * from './style';
