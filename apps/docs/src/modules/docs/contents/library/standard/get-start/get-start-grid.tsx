334f7b10e62437facf8d28bbfef6c9877594be33
pionpill <673486387@qq.com>
2026-09-22 10:18:18 +0800

    📝 docs: 优化 Standard 快速开始
    补充可视化预览与 React/Vanilla 分页接入说明，并明确上层包文档的独立阅读约束。
    
    Refs: module=docs; packages=docs,standard; version=-; adr=-\nControl: human-directed

---
+++ apps/docs/src/modules/docs/contents/library/standard/get-start/get-start-grid.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@retikz/react';
+import { Grid } from '@retikz/standard-react';
+import type { FC } from 'react';
+
+const GetStartGrid: FC = () => (
+  <Layout>
+    <Grid bounds={{ start: [0, 0], end: [240, 120] }} line={{ spacing: 20 }} />
+  </Layout>
+);
+
+export default GetStartGrid;
